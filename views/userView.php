1a4408f91c012a5dce2610425c3e6c8d66b6124c
GabrielGamboa-Codex <foxygamboafnaf2003@gmail.com>
2024-11-01 14:57:21 -0400

    espacios

--- views/userView.php
+++ views/userView.php
@@ -42,6 +42,10 @@
     <br>
     <br>
 
+
+
+
+    
     <!-- Modal Crear Usuarios -->
     <div class="modal fade" id="createUsermodal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="createUsermodal" aria-hidden="true">
         <div class="modal-dialog">
